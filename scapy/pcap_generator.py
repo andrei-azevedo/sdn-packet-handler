0ec606ca589c40ee484ecda2251511d6febeaa9c
Andrei Azevedo <deicordova@hotmail.com>
2023-08-21 22:15:04 -0300

    generate pcap files using scapy

---
+++ scapy/pcap_generator.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+from scapy.layers.inet import IP, TCP
+
+def main():
+    while(True):
+        pkt, _  = sr(IP(dst="10.0.0.2")/TCP(dport=80)/Raw(RandString(size=random.randint(72, 150))))
+        write_to_pcap(pkt)
+        time.sleep(0.1)
+
+def write_to_pcap(pkt):
+    wrpcap("temp.pcap", pkt, append=True)
+    
+if __name__ == "__main__":
+    main()
